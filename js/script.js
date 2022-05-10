5f332550a7a3b3e51f5df03c43b74d2747ad430d
Mick Pitzer <mpitzer01@pm.me>
2022-05-09 20:02:20 -0400

    third commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// HAMBURGER SCRIPT
+let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.navbar');
+let bod = document.querySelector('.container');
+
+hamburger.addEventListener('click', function() {
+  hamburger.classList.toggle('isactive');
+  menu.classList.toggle('active');
+
+});
